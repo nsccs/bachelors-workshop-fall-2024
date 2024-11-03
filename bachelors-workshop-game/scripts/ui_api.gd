8f3ae46718d91fa8ac2e106f78a7f81597ebeb83
bennext <Benjamin.Guth2@Seattlecolleges.edu>
2024-11-02 18:33:59 -0700

    UI now has a health bar and a stamina bar

--- bachelors-workshop-game/scripts/ui_api.gd
+++ bachelors-workshop-game/scripts/ui_api.gd
@@ -1,6 +1,9 @@
 extends Node
 class_name UI
 @onready var stamina_label = $CanvasLayer/PanelContainer/MarginContainer/GridContainer/StaminaLabel
+@onready var heakth_bar = $CanvasLayer/HealthBar
+
+
 
 func set_stamina_bar(stamina, max_stamina):
 	stamina_label.text = "stamina: "+stamina+"/" +max_stamina
@@ -13,4 +16,35 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$CanvasLayer/TimePanelContainer3/MarginContainer/TimeLabel.text = "%s" % roundf($CanvasLayer/TimePanelalvContainer3/Timer.time_left )
+	# That's the timer 
 	pass
+
+# func update():
+	 
+
+#healh bar functions 
+func change_health_bar(hp):
+	$CanvasLayer/HealthBar.value = hp
+	
+func damage_health_bar(damage):
+	$CanvasLayer/HealthBar.value -= damage
+	
+func heal_health_bar(heal):
+	$CanvasLayer/HealthBar.value += heal
+	
+func current_health_bar():
+	return $CanvasLayer/HealthBar.value
+	
+#stammina bar functions 
+func change_stamina_bar(new_value):
+	$CanvasLayer/staminaBar.value = new_value
+	
+func damage_stamina_bar(ex):
+	$CanvasLayer/staminaBar.value -= ex
+	
+func heal_stamina_bar(recharge):
+	$CanvasLayer/staminaBar.value += recharge
+	
+func current_stamina_bar():
+	return $CanvasLayer/staminaBar.value
