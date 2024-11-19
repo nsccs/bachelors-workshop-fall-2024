43f81219e3ba666c87b15512e5e1a2335377bcd9
mizotii <omphen@uw.edu>
2024-11-18 20:04:58 -0800

    (tentative, uses too much memory) world now generates fully

--- bachelors-workshop-game/scripts/game_world.gd
+++ bachelors-workshop-game/scripts/game_world.gd
@@ -1,9 +1,9 @@
 extends Node3D
 class_name GameWorld
 
-const MAP_SIZE = 400 # map is a square, this is effectively its length and width in tiles.
+const MAP_SIZE = 200 # map is a square, this is effectively its length and width in tiles.
 const TILE_SIZE = 32 
-const BUILDING_COUNT = 30
+const BUILDING_COUNT = 20
 const PUDDLE_FACTOR = 0.2 # this number needs playing around with. just know that higher = higher likelihood
 						  # of puddles instantiating
 const SPLIT_ITERATIONS = 5 # will produce at maximum 2^n 'boxes' which buildings can fill
@@ -13,7 +13,8 @@ const MIN_SPACE_BETWEEN_BUILDING_AND_STREET = 6 # ensures buildings don't touch 
 												# how big our prefab buildings are
 
 var rng = RandomNumberGenerator.new()
-var filled_locations = [] # every entry is a location where a generated tile is
+var filled_locations = [] # every entry is a location where a generated tile is. format:
+						  # int x, int x, boolean tileExists
 
 enum BuildingType {
 	SKYSCRAPER,
@@ -50,8 +51,6 @@ enum TileType {
 # @export var fire_station: PackedScene
 
 func generate_world():
-	# default
-	generate_default()
 	
 	# border
 	generate_rubble()
@@ -64,7 +63,7 @@ func generate_world():
 		# ensures buildings are min spaces away from the border and from every street tile
 		var x = rng.randi_range(MIN_SPACE_BETWEEN_BUILDING_AND_STREET, MAP_SIZE - MIN_SPACE_BETWEEN_BUILDING_AND_STREET)
 		var y = rng.randi_range(MIN_SPACE_BETWEEN_BUILDING_AND_STREET, MAP_SIZE - MIN_SPACE_BETWEEN_BUILDING_AND_STREET)
-		while filled_locations.has([x, y]):
+		while filled_locations.has([x, y, true]):
 			x = rng.randi_range(MIN_SPACE_BETWEEN_BUILDING_AND_STREET, MAP_SIZE - MIN_SPACE_BETWEEN_BUILDING_AND_STREET)
 			y = rng.randi_range(MIN_SPACE_BETWEEN_BUILDING_AND_STREET, MAP_SIZE - MIN_SPACE_BETWEEN_BUILDING_AND_STREET)
 		
@@ -74,8 +73,13 @@ func generate_world():
 		# ensures buildings are min spaces away from each other
 		for i in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
 			for j in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
-				filled_locations.append([x + i, y + j])
+				if i != 0 && j != 0:
+					filled_locations.append([x + i, y + j, false])
+				else:
+					filled_locations.append([x + i, y + j, true])
 		generate_building(x, y, rng.randi_range(0, BuildingType.size() - 1))
+		
+		generate_remainder()
 
 # todo
 func destroy_world():
@@ -117,18 +121,25 @@ func split(iterations: int, direction: int, x_min: int, x_max: int, y_min: int, 
 		# don't want to create divides too close to edge
 		var position = rng.randi_range(x_min + (x_space / GRID_NOISE), x_max - (x_space / GRID_NOISE))
 		for i in range(y_min, y_max):
-			generate_tile(position, i, -1, TileType.MEDIAN)
+			generate_tile(position, i, 0.0, TileType.MEDIAN)
 			for j in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
-				filled_locations.append([position + j, i])
+				if j != 0:
+					filled_locations.append([position + j, i, false])
+				else:
+					filled_locations.append([position + j, i, true])
+					
 		split(iterations - 1, 1, x_min, position, y_min, y_max)
 		split(iterations - 1, 1, position, x_max, y_min, y_max)
 	# split along y
 	else:
 		var position = rng.randi_range(y_min + (y_space / GRID_NOISE), y_max - (y_space / GRID_NOISE))
 		for i in range(x_min, x_max):
-			generate_tile(i, position, -1, TileType.MEDIAN)
+			generate_tile(i, position, 0.0, TileType.MEDIAN)
 			for j in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
-				filled_locations.append([i, position + j])
+				if j != 0:
+					filled_locations.append([i, position + j, false])
+				else:
+					filled_locations.append([i, position + j, true])
 		split(iterations - 1, 0, x_min, x_max, y_min, position)
 		split(iterations - 1, 0, x_min, x_max, position, y_max)
 
@@ -136,14 +147,14 @@ func split(iterations: int, direction: int, x_min: int, x_max: int, y_min: int, 
 # todo: why is it not rubble? also create algo to spawn rubble in sightlines
 func generate_rubble():
 	for i in MAP_SIZE:
-		generate_tile(i, 0, -1, TileType.RUBBLE)
-		filled_locations.append([i, 0])
-		generate_tile(i, MAP_SIZE, -1, TileType.RUBBLE)
-		filled_locations.append([i, MAP_SIZE])
-		generate_tile(0, i, -1, TileType.RUBBLE)
-		filled_locations.append([0, i])
-		generate_tile(MAP_SIZE, i, -1, TileType.RUBBLE)
-		filled_locations.append([MAP_SIZE, i])
+		generate_tile(i, 0, 0.0, TileType.RUBBLE)
+		filled_locations.append([i, 0, true])
+		generate_tile(i, MAP_SIZE, 0.0, TileType.RUBBLE)
+		filled_locations.append([i, MAP_SIZE, true])
+		generate_tile(0, i, 0.0, TileType.RUBBLE)
+		filled_locations.append([0, i, true])
+		generate_tile(MAP_SIZE, i, 0.0, TileType.RUBBLE)
+		filled_locations.append([MAP_SIZE, i, true])
 
 # adds a tile node to the world
 func generate_tile(x: int, y: int, z: int, type: TileType = TileType.NONE):
@@ -167,10 +178,13 @@ func generate_tile(x: int, y: int, z: int, type: TileType = TileType.NONE):
 			rubble.position = Vector3(x, y, z)
 			add_child(rubble)
 
-# todo: where there are no tiles after other generations finished, fill with
+# where there are no tiles after other generations finished, fill with
 # 'default' street type tiles
-func generate_default():
-	pass
+func generate_remainder():
+	for x in MAP_SIZE:
+		for y in MAP_SIZE:
+			if !filled_locations.has([x, y, true]):
+				generate_tile(x, y, 0.0, TileType.STREET)
 
 # might be deprecated, would be used to generate either a 'default' tile or
 # puddle tile
