80d7ca94b0d6dd1a3d0ce4f3e11bfab89abafa95
mizotii <omphen@uw.edu>
2024-11-18 19:08:58 -0800

    make all tiles and building types their own separate node

---
+++ bachelors-workshop-game/scripts/buildings/restaurant.gd
@@ -0,0 +1,13 @@
+extends Node3D
+class_name Restaurant
+
+@onready var instance := $MeshInstance3D
+
+# again, prob a better way to do this but just trying to make it work
+func set_mesh_color(instance: MeshInstance3D, color: Color):
+	var mesh = instance.mesh
+	for surface in mesh.get_surface_count():
+		mesh.surface_get_material(surface).albedo_color = color
+
+func _ready():
+	set_mesh_color(instance, "#E6E6E6")
--- bachelors-workshop-game/scripts/game_world.gd
+++ bachelors-workshop-game/scripts/game_world.gd
@@ -15,9 +15,39 @@ const MIN_SPACE_BETWEEN_BUILDING_AND_STREET = 6 # ensures buildings don't touch 
 var rng = RandomNumberGenerator.new()
 var filled_locations = [] # every entry is a location where a generated tile is
 
+enum BuildingType {
+	SKYSCRAPER,
+	APARTMENT,
+	RESTAURANT,
+	# FACTORY,
+	# POLICE_STATION,
+	# FIRE_STATION
+}
+
+enum TileType {
+	NONE,
+	STREET,
+	MEDIAN,
+	PUDDLE,
+	RUBBLE
+}
+
 @export var noise: FastNoiseLite
-@export var tile: PackedScene
-@export var building: PackedScene
+
+# ground tiles
+@export var none: PackedScene
+@export var street: PackedScene
+@export var median: PackedScene
+@export var puddle: PackedScene
+@export var rubble: PackedScene
+
+# buildings
+@export var skyscraper: PackedScene
+@export var apartment: PackedScene
+@export var restaurant: PackedScene
+# @export var factory: PackedScene
+# @export var police_station: PackedScene
+# @export var fire_station: PackedScene
 
 func generate_world():
 	# default
@@ -45,7 +75,7 @@ func generate_world():
 		for i in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
 			for j in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
 				filled_locations.append([x + i, y + j])
-		generate_building(x, y, rng.randi_range(0, Building.BuildingType.size() - 1))
+		generate_building(x, y, rng.randi_range(0, BuildingType.size() - 1))
 
 # todo
 func destroy_world():
@@ -54,24 +84,20 @@ func destroy_world():
 # instantiates a building scene (as of now, a 1x1x1 tile)
 # todo: figure out why it spits out the same type no matter what type is passed
 func generate_building(x: int, y: int, type: int):
-	var building = building.instantiate()
 	# there's gotta be a better way to do this but...
 	match type:
 		0:
-			building.building_type = Building.BuildingType.SKYSCRAPER
+			var skyscraper = skyscraper.instantiate()
+			skyscraper.position = Vector3(x, y, 0.0)
+			add_child(skyscraper)
 		1:
-			building.building_type = Building.BuildingType.APARTMENT
+			var apartment = apartment.instantiate()
+			apartment.position = Vector3(x, y, 0.0)
+			add_child(apartment)
 		2:
-			building.building_type = Building.BuildingType.RESTAURANT
-		3:
-			building.building_type = Building.BuildingType.FACTORY
-		4:
-			building.building_type = Building.BuildingType.POLICE_STATION
-		5:
-			building.building_type = Building.BuildingType.FIRE_STATION
-		
-	building.position = Vector3(x, y, 0.0)
-	add_child(building)
+			var restaurant = restaurant.instantiate()
+			restaurant.position = Vector3(x, y, 0.0)
+			add_child(restaurant)
 
 func generate_roads(iterations: int):
 	split(iterations, 0, 0, MAP_SIZE, 0, MAP_SIZE)
@@ -91,7 +117,7 @@ func split(iterations: int, direction: int, x_min: int, x_max: int, y_min: int, 
 		# don't want to create divides too close to edge
 		var position = rng.randi_range(x_min + (x_space / GRID_NOISE), x_max - (x_space / GRID_NOISE))
 		for i in range(y_min, y_max):
-			generate_tile(position, i, -1, Tile.TileType.MEDIAN)
+			generate_tile(position, i, -1, TileType.MEDIAN)
 			for j in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
 				filled_locations.append([position + j, i])
 		split(iterations - 1, 1, x_min, position, y_min, y_max)
@@ -100,7 +126,7 @@ func split(iterations: int, direction: int, x_min: int, x_max: int, y_min: int, 
 	else:
 		var position = rng.randi_range(y_min + (y_space / GRID_NOISE), y_max - (y_space / GRID_NOISE))
 		for i in range(x_min, x_max):
-			generate_tile(i, position, -1, Tile.TileType.MEDIAN)
+			generate_tile(i, position, -1, TileType.MEDIAN)
 			for j in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
 				filled_locations.append([i, position + j])
 		split(iterations - 1, 0, x_min, x_max, y_min, position)
@@ -110,24 +136,36 @@ func split(iterations: int, direction: int, x_min: int, x_max: int, y_min: int, 
 # todo: why is it not rubble? also create algo to spawn rubble in sightlines
 func generate_rubble():
 	for i in MAP_SIZE:
-		generate_tile(i, 0, -1, Tile.TileType.RUBBLE)
+		generate_tile(i, 0, -1, TileType.RUBBLE)
 		filled_locations.append([i, 0])
-		generate_tile(i, MAP_SIZE, -1, Tile.TileType.RUBBLE)
+		generate_tile(i, MAP_SIZE, -1, TileType.RUBBLE)
 		filled_locations.append([i, MAP_SIZE])
-		generate_tile(0, i, -1, Tile.TileType.RUBBLE)
+		generate_tile(0, i, -1, TileType.RUBBLE)
 		filled_locations.append([0, i])
-		generate_tile(MAP_SIZE, i, -1, Tile.TileType.RUBBLE)
+		generate_tile(MAP_SIZE, i, -1, TileType.RUBBLE)
 		filled_locations.append([MAP_SIZE, i])
 
 # adds a tile node to the world
-func generate_tile(x: int, y: int, z: int, type: Tile.TileType = Tile.TileType.NONE):
-	var tile = tile.instantiate()
-	if type == Tile.TileType.NONE:
-		tile.tile_type = get_puddle_or_street(x, y)
-	else:
-		tile.tile_type = type
-	tile.position = Vector3(x, y, z)
-	add_child(tile)
+func generate_tile(x: int, y: int, z: int, type: TileType = TileType.NONE):
+	if type == TileType.NONE:
+		type = get_puddle_or_street(x, y)
+	match type:
+		TileType.STREET:
+			var street = street.instantiate()
+			street.position = Vector3(x, y, z)
+			add_child(street)
+		TileType.MEDIAN:
+			var median = median.instantiate()
+			median.position = Vector3(x, y, z)
+			add_child(median)
+		TileType.PUDDLE:
+			var puddle = puddle.instantiate()
+			puddle.position = Vector3(x, y, z)
+			add_child(puddle)
+		TileType.RUBBLE:
+			var rubble = rubble.instantiate()
+			rubble.position = Vector3(x, y, z)
+			add_child(rubble)
 
 # todo: where there are no tiles after other generations finished, fill with
 # 'default' street type tiles
@@ -136,13 +174,13 @@ func generate_default():
 
 # might be deprecated, would be used to generate either a 'default' tile or
 # puddle tile
-func get_puddle_or_street(x: int, y: int) -> Tile.TileType:
+func get_puddle_or_street(x: int, y: int) -> TileType:
 	var value = noise.get_noise_2d(x, y)
 	
 	if value >= PUDDLE_FACTOR:
-		return Tile.TileType.STREET
+		return TileType.STREET
 		
-	return Tile.TileType.PUDDLE
+	return TileType.PUDDLE
 
 func _ready():
 	generate_world()
