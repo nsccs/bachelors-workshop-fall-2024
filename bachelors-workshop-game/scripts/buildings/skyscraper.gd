93bd061ecb267be4d9ae1f1adf66380adfba909d
mizotii <omphen@uw.edu>
2024-11-25 22:35:05 -0800

    fix map orientation, fix buildings being surrounded by air, add customizable depth to skyscrapers

--- bachelors-workshop-game/scripts/buildings/skyscraper.gd
+++ bachelors-workshop-game/scripts/buildings/skyscraper.gd
@@ -1,6 +1,10 @@
 extends Node3D
 class_name Skyscraper
 
+const SIZE_X = 3
+const SIZE_Y = GameWorld.SKYSCRAPER_HEIGHT
+const SIZE_Z = 3
+
 @onready var instance := $MeshInstance3D
 
 # again, prob a better way to do this but just trying to make it work
@@ -8,6 +12,10 @@ func set_mesh_color(instance: MeshInstance3D, color: Color):
 	var mesh = instance.mesh
 	for surface in mesh.get_surface_count():
 		mesh.surface_get_material(surface).albedo_color = color
+		
+func set_mesh_size(instance: MeshInstance3D, size_x: int, size_y: int, size_z: int):
+	instance.mesh.set_size(Vector3(SIZE_X, SIZE_Y, SIZE_Z))
 
 func _ready():
 	set_mesh_color(instance, "#808080")
+	set_mesh_size(instance, SIZE_X, SIZE_Y, SIZE_Z)
--- bachelors-workshop-game/scripts/game_world.gd
+++ bachelors-workshop-game/scripts/game_world.gd
@@ -3,7 +3,7 @@ class_name GameWorld
 
 const MAP_SIZE = 100 # map is a square, this is effectively its length and width in tiles.
 const TILE_SIZE = 32 
-const BUILDING_COUNT = 20
+const BUILDING_COUNT = 10
 const PUDDLE_FACTOR = 0.2 # this number needs playing around with. just know that higher = higher likelihood
 						  # of puddles instantiating
 const SPLIT_ITERATIONS = 5 # will produce at maximum 2^n 'boxes' which buildings can fill
@@ -11,6 +11,8 @@ const GRID_NOISE = 1.4 # this gives us a grid not too chaotic but not too simple
 const MIN_SPACE_BETWEEN_ROADS_FACTOR = 4 # ensures we don't get parallel roads super close to one another
 const MIN_SPACE_BETWEEN_BUILDING_AND_STREET = 6 # ensures buildings don't touch streets. depends on
 												# how big our prefab buildings are
+												
+const SKYSCRAPER_HEIGHT = 30
 
 var rng = RandomNumberGenerator.new()
 var filled_locations = [] # every entry is a location where a generated tile is. format:
@@ -62,10 +64,10 @@ func generate_world():
 	for n in BUILDING_COUNT:
 		# ensures buildings are min spaces away from the border and from every street tile
 		var x = rng.randi_range(MIN_SPACE_BETWEEN_BUILDING_AND_STREET, MAP_SIZE - MIN_SPACE_BETWEEN_BUILDING_AND_STREET)
-		var y = rng.randi_range(MIN_SPACE_BETWEEN_BUILDING_AND_STREET, MAP_SIZE - MIN_SPACE_BETWEEN_BUILDING_AND_STREET)
-		while filled_locations.has([x, y, true]):
+		var z = rng.randi_range(MIN_SPACE_BETWEEN_BUILDING_AND_STREET, MAP_SIZE - MIN_SPACE_BETWEEN_BUILDING_AND_STREET)
+		while filled_locations.has([x, z, true]) || filled_locations.has([x, z, false]):
 			x = rng.randi_range(MIN_SPACE_BETWEEN_BUILDING_AND_STREET, MAP_SIZE - MIN_SPACE_BETWEEN_BUILDING_AND_STREET)
-			y = rng.randi_range(MIN_SPACE_BETWEEN_BUILDING_AND_STREET, MAP_SIZE - MIN_SPACE_BETWEEN_BUILDING_AND_STREET)
+			z = rng.randi_range(MIN_SPACE_BETWEEN_BUILDING_AND_STREET, MAP_SIZE - MIN_SPACE_BETWEEN_BUILDING_AND_STREET)
 		
 		# maybe we can weigh different building types later, but for now each 
 		# building has a random chance of being drawn
@@ -73,11 +75,8 @@ func generate_world():
 		# ensures buildings are min spaces away from each other
 		for i in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
 			for j in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
-				if i != 0 && j != 0:
-					filled_locations.append([x + i, y + j, false])
-				else:
-					filled_locations.append([x + i, y + j, true])
-		generate_building(x, y, rng.randi_range(0, BuildingType.size() - 1))
+				filled_locations.append([x + i, z + j, false])
+		generate_building(x, z, rng.randi_range(0, BuildingType.size() - 1))
 		
 	generate_remainder()
 
@@ -87,20 +86,20 @@ func destroy_world():
 
 # instantiates a building scene (as of now, a 1x1x1 tile)
 # todo: figure out why it spits out the same type no matter what type is passed
-func generate_building(x: int, y: int, type: int):
+func generate_building(x: int, z: int, type: int):
 	# there's gotta be a better way to do this but...
 	match type:
 		0:
 			var skyscraper = skyscraper.instantiate()
-			skyscraper.position = Vector3(x, y, 0.0)
+			skyscraper.position = Vector3(x, SKYSCRAPER_HEIGHT / 2, z)
 			add_child(skyscraper)
 		1:
 			var apartment = apartment.instantiate()
-			apartment.position = Vector3(x, y, 0.0)
+			apartment.position = Vector3(x, 1, z)
 			add_child(apartment)
 		2:
 			var restaurant = restaurant.instantiate()
-			restaurant.position = Vector3(x, y, 0.0)
+			restaurant.position = Vector3(x, 1, z)
 			add_child(restaurant)
 
 func generate_roads(iterations: int):
@@ -108,53 +107,54 @@ func generate_roads(iterations: int):
 
 # recursively 'splits' the map in half by placing street tiles in straight lines
 # in either the x or y direction, from the given grids end to other end
-func split(iterations: int, direction: int, x_min: int, x_max: int, y_min: int, y_max: int):
+func split(iterations: int, direction: int, x_min: int, x_max: int, z_min: int, z_max: int):
 	if iterations == 0:
 		return
 	var x_space = x_max - x_min
-	var y_space = y_max - y_min
-	if x_space < MAP_SIZE / MIN_SPACE_BETWEEN_ROADS_FACTOR or y_space < MAP_SIZE / MIN_SPACE_BETWEEN_ROADS_FACTOR:
+	var z_space = z_max - z_min
+	if x_space < MAP_SIZE / MIN_SPACE_BETWEEN_ROADS_FACTOR or z_space < MAP_SIZE / MIN_SPACE_BETWEEN_ROADS_FACTOR:
 		return
 	
 	# split along x
 	if direction == 0:
 		# don't want to create divides too close to edge
 		var position = rng.randi_range(x_min + (x_space / GRID_NOISE), x_max - (x_space / GRID_NOISE))
-		for i in range(y_min, y_max):
-			generate_tile(position, i, 0.0, TileType.MEDIAN)
+		for i in range(z_min, z_max):
+			generate_tile(position, 0, i, TileType.MEDIAN)
 			for j in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
 				if j != 0:
 					filled_locations.append([position + j, i, false])
 				else:
 					filled_locations.append([position + j, i, true])
 					
-		split(iterations - 1, 1, x_min, position, y_min, y_max)
-		split(iterations - 1, 1, position, x_max, y_min, y_max)
-	# split along y
+		split(iterations - 1, 1, x_min, position, z_min, z_max)
+		split(iterations - 1, 1, position, x_max, z_min, z_max)
+	# split along z
 	else:
-		var position = rng.randi_range(y_min + (y_space / GRID_NOISE), y_max - (y_space / GRID_NOISE))
+		var position = rng.randi_range(z_min + (z_space / GRID_NOISE), z_max - (z_space / GRID_NOISE))
 		for i in range(x_min, x_max):
-			generate_tile(i, position, 0.0, TileType.MEDIAN)
+			generate_tile(i, 0, position, TileType.MEDIAN)
 			for j in range((MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2) * -1, MIN_SPACE_BETWEEN_BUILDING_AND_STREET / 2):
 				if j != 0:
 					filled_locations.append([i, position + j, false])
 				else:
 					filled_locations.append([i, position + j, true])
-		split(iterations - 1, 0, x_min, x_max, y_min, position)
-		split(iterations - 1, 0, x_min, x_max, position, y_max)
+		split(iterations - 1, 0, x_min, x_max, z_min, position)
+		split(iterations - 1, 0, x_min, x_max, position, z_max)
 
 # as of now, just generates the border
 # todo: why is it not rubble? also create algo to spawn rubble in sightlines
 func generate_rubble():
 	for i in MAP_SIZE:
-		generate_tile(i, 0, 0.0, TileType.RUBBLE)
+		generate_tile(i, 0, 0, TileType.RUBBLE)
 		filled_locations.append([i, 0, true])
-		generate_tile(i, MAP_SIZE, 0.0, TileType.RUBBLE)
+		generate_tile(i, 0, MAP_SIZE, TileType.RUBBLE)
 		filled_locations.append([i, MAP_SIZE, true])
-		generate_tile(0, i, 0.0, TileType.RUBBLE)
+		generate_tile(0, 0, i, TileType.RUBBLE)
 		filled_locations.append([0, i, true])
-		generate_tile(MAP_SIZE, i, 0.0, TileType.RUBBLE)
+		generate_tile(MAP_SIZE, 0, i, TileType.RUBBLE)
 		filled_locations.append([MAP_SIZE, i, true])
+	
 
 # adds a tile node to the world
 func generate_tile(x: int, y: int, z: int, type: TileType = TileType.NONE):
@@ -182,14 +182,14 @@ func generate_tile(x: int, y: int, z: int, type: TileType = TileType.NONE):
 # 'default' street type tiles
 func generate_remainder():
 	for x in MAP_SIZE:
-		for y in MAP_SIZE:
-			if !filled_locations.has([x, y, true]):
-				generate_tile(x, y, 0.0, TileType.STREET)
+		for z in MAP_SIZE:
+			if !filled_locations.has([x, z, true]):
+				generate_tile(x, 0, z, TileType.STREET)
 
 # might be deprecated, would be used to generate either a 'default' tile or
 # puddle tile
-func get_puddle_or_street(x: int, y: int) -> TileType:
-	var value = noise.get_noise_2d(x, y)
+func get_puddle_or_street(x: int, z: int) -> TileType:
+	var value = noise.get_noise_2d(x, z)
 	
 	if value >= PUDDLE_FACTOR:
 		return TileType.STREET
